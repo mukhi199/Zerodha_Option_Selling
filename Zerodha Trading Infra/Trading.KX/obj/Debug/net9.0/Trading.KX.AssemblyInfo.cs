10e4af0cd95c5ace3af8737ff74418bc506791cf
mukteshwar <mukteshwar@gmail.com>
2026-05-09 14:19:07 +0530

    feat: implement automated Zerodha login and instrument synchronization flow

--- Zerodha Trading Infra/Trading.KX/obj/Debug/net9.0/Trading.KX.AssemblyInfo.cs
+++ Zerodha Trading Infra/Trading.KX/obj/Debug/net9.0/Trading.KX.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trading.KX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cc0102b60397aec69dc3350c54840b582729736")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6c59cb4f837604c2711c6874c1dc5f225de1cf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trading.KX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trading.KX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
